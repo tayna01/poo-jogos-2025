e3dc100a018020c24e2a0ca7a702695b1f79ef33
Tayna <taynavicente2019@gmail.com>
2025-04-09 21:35:59 -0300

    chore: adiciona classe Main para execução do jogo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import controller.JogoCacaPalavrasController;
+import model.Usuario;
+import view.JogoCacaPalavrasView;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        Usuario usuario = new Usuario(nome);
+        JogoCacaPalavrasView view = new JogoCacaPalavrasView();
+        JogoCacaPalavrasController controller = new JogoCacaPalavrasController(usuario, view);
+
+        controller.iniciarJogo();
     }
 }
